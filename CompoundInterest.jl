f2f7dc92182bc64dd843eef45bb4a578a69b1de2
JJ Garzella <jjgarzella@gmail.com>
2020-10-11 09:15:35 -0700

    First commit

---
+++ CompoundInterest.jl
@@ -0,0 +1,15 @@
+
+module CompoundInterest
+
+using NiLang
+
+@i function compound_interest(principal,rate,years)
+  for i = 1:years
+    for j = 1:12
+      temp 
+      money += (rate / 12)
+    end
+  end
+end
+
+end#module
